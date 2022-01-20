c92908aa705ba69c1f925795fc683652eaa4aca3
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-01-20 15:53:29 -0500

    original migration used non real so update to real

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,13 @@
 class Helpers
+
+    def self.current_user(session)
+        #uses session hash to search in database if a current user.
+        @user = User.find_by_id(session[:user_id])
+    end
+
+    def self.is_logged_in?(session)
+        # returns true if the user_id is in the session hash and false if not.
+        !!session[:user_id]
+    end
+
 end
